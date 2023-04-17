fbfa1ad9035fdec9ca8816534a66b57ff5f01ef7
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-04-17 15:54:02 +0500

    Update robocontest_masala.py

--- robocontest_masala.py
+++ robocontest_masala.py
@@ -74,6 +74,30 @@ n.remove(max(n))
 print(max(n))
 
 
+"masala-12"
+def tub(n):
+    a = []
+    for i in range(n + 1):a.append(i)
+    a[1] = 0; i = 2
+    while i <= n:
+        if a[i] != 0:
+            j = i + i
+            while j <= n:
+                a[j] = 0
+                j += i
+        i += 1
+    a = set(a)
+    a.remove(0)
+    return a
+n = int(input())
+a = tub(n)
+ism=["Bobur","Ali"]
+if n==1:
+  print("Ali")
+else:
+  print(ism[(len(a)%2)])
+
+
 "masala-13"
 n,k = map(int,input().split())
 if n > 0 :
