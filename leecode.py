8a4da71c0aec1ee5817452e7e48f705037f8f2b1
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-12-23 01:24:32 +0500

    feat: leetcode solution-1

--- leecode.py
+++ leecode.py
@@ -192,4 +192,13 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
 
+
+# 1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
         
