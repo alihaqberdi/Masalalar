abebb34efdb9b2ac2357a52e1c589c930431f161
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-12-23 01:20:56 +0500

    feat: leetcode solution-13

--- leecode.py
+++ leecode.py
@@ -172,4 +172,19 @@ class Solution:
         return max_length
 
 
+# 13
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        pre = 0
+        res = 0
+        for i in s:
+            v = val[i]
+            if pre < v:
+                res -= pre*2
+            res += v
+            pre = v
+        return res
+
+
         
