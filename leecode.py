00b6e44e5a376cb1e862826b3b406f3f0def62bd
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-12-22 20:49:59 +0500

    feat: 1920 solution

--- leecode.py
+++ leecode.py
@@ -1,3 +1,11 @@
+
+# 1108
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return address.replace(".", "[.]")
+
+
+# 1920
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[i] for i in nums]
