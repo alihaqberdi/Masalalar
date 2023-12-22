179a930f630fc60318ca45312cd58ddcddd1090b
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-12-22 21:02:37 +0500

    feat: solution 2011

--- leecode.py
+++ leecode.py
@@ -1,4 +1,3 @@
-
 # 1108
 class Solution:
     def defangIPaddr(self, address: str) -> str:
@@ -9,3 +8,15 @@ class Solution:
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         return [nums[i] for i in nums]
+
+
+# 2011
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for i in operations:
+            if "--" in i:
+                x -= 1
+                continue
+            x+=1
+        return x
