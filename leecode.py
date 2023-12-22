eb71e54dc9e63590c9e813b74f964217f74b2a8a
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-12-23 01:17:22 +0500

    feat: leetcode solution-3

--- leecode.py
+++ leecode.py
@@ -153,4 +153,23 @@ class Solution:
                     return -1
 
 
+# 3
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        char_set = set()
+        max_length = 0
+        left, right = 0, 0
+        while right < len(s):
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                right += 1
+                max_length = max(max_length, right - left)
+            else:
+                char_set.remove(s[left])
+                left += 1
+        return max_length
+
+
         
