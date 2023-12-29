37fd002183fa4682d5bf51ae3f870ead4a4ace86
Alijon Xakberdiyev <107967097+alihaqberdi@users.noreply.github.com>
2023-12-29 10:40:18 +0500

    feat: leetcode solution-2894

--- leecode.py
+++ leecode.py
@@ -319,3 +319,16 @@ class Solution:
             if i >= target:
                 res += 1
         return res
+
+
+# 2894
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = 0
+        num2 = 0
+        for i in range(1,n+1):
+            if i % m:
+                num1 += i
+                continue
+            num2 += i
+        return num1 -num2 
